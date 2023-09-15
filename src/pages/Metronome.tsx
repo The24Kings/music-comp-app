cd2e59f6ea9ca0be97fba313d86bad62349a3872
Tyler <t.drover2001@gmail.com>
2023-09-15 14:44:23 -0700

    Animation

--- src/pages/Metronome.tsx
+++ src/pages/Metronome.tsx
@@ -1,23 +1,29 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import MetronomeContainer from '../components/MetronomeContainer';
+import ImageSlideshow from '../components/ImageSlideshow';
 
 const Metronome: React.FC = () => {
     return (
-      <IonPage>
-        <IonHeader>
-          <IonToolbar>
-            <IonTitle>Metronome</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <IonContent fullscreen>
-          <IonHeader collapse="condense">
-            <IonToolbar>
-              <IonTitle size="large">Metronome</IonTitle>
-            </IonToolbar>
-          </IonHeader>
-          <MetronomeContainer name="Metronome page" />
-        </IonContent>
-      </IonPage>
+        <IonPage>
+            <IonHeader>
+                <IonToolbar>
+                    <IonTitle>Metronome</IonTitle>
+                </IonToolbar>
+            </IonHeader>
+            <IonContent fullscreen>
+                <IonHeader collapse="condense">
+                    <IonToolbar>
+                        <IonTitle size="large">Metronome</IonTitle>
+                    </IonToolbar>
+                </IonHeader>
+                <MetronomeContainer name="Metronome page" />
+
+                {/* Wrap ImageSlideshow in a centered container */}
+                <div className="centered-container">
+                    <ImageSlideshow />
+                </div>
+            </IonContent>
+        </IonPage>
     );
 };
 

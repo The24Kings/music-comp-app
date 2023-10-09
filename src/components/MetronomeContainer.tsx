b21fa23fdb0602bfeba74904582e25bc8fcd7336
24Kings <24kingsunite@gmail.com>
2023-10-09 12:44:48 -0700

    metronome arm now updates using useAnimationFrame

--- src/components/MetronomeContainer.tsx
+++ src/components/MetronomeContainer.tsx
@@ -1,39 +1,84 @@
 import './MetronomeContainer.css';
 
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef, useCallback } from 'react';
 import { IonButton, IonButtons, IonContent, IonInput, IonItem, IonLabel, IonToolbar } from '@ionic/react';
 import sound from "/assets/sounds/metronomeSound2.mp3";
 
 interface ContainerProps {
     name: string;
+    time: float;
+}
+
+export const useAnimationFrame = (
+  cb: (arg: { time: number; delta: number }) => void,
+  deps: readonly unknown[],
+) => {
+  /* eslint-disable react-hooks/rules-of-hooks */
+  if (typeof window === 'undefined') return
+
+  const frame = useRef<number>()
+  const last = useRef(performance.now())
+  const init = useRef(performance.now())
+
+  const animate = useCallback(() => {
+    const now = performance.now()
+    const time = (now - init.current) / 1_000
+    const delta = (now - last.current) / 1_000
+
+    // eslint-disable-next-line n/callback-return, n/no-callback-literal, promise/prefer-await-to-callbacks
+    cb({ time, delta })
+
+    last.current = now
+    frame.current = requestAnimationFrame(animate)
+  }, [cb])
+
+  useEffect(() => {
+    frame.current = requestAnimationFrame(animate)
+    return () => {
+      if (frame.current) cancelAnimationFrame(frame.current)
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [animate, ...deps])
+  /* eslint-enable react-hooks/rules-of-hooks */
 }
 
 const MetronomeContainer: React.FC<ContainerProps> = ({ name }) => {
-    const metronomeBase = '../assets/pictures/metronome.svg';
-    const metronomeArm = '../assets/pictures/arm.svg';
+    const metronomeBase = '../assets/pictures/metronome-grey.svg';
+    const metronomeArm = '../assets/pictures/arm-white.svg';
 
-    const [isRunning, setIsRunning] = useState(true);
+    const [isRunning, setIsRunning] = useState(false);
     const [rads, setRads] = useState(0);
-    const [frequency, setFrequency] = useState(120/60);
-    const [bpm, setBpm] = useState(60); // Initial BPM value
+    const [bpm, setBpm] = useState(120); // Initial BPM value
+    const [frequency, setFrequency] = useState(bpm/60);
     const audioRef = useRef<HTMLAudioElement | null>(null);
 
-    const amplitude = 10; // Smoothness of the animation -> Placeholder
     const time = Date.now();
+    const armRef = useRef<HTMLImageElement>(null)
 
-    //Thinks it is event driven, maybe, please look into
-    useEffect(() => {
-         if(isRunning) {
-            const currentTime = Date.now();
+    useAnimationFrame(({ time }) => {
+        const img = armRef.current
 
-            const frequency = bpm / 60;
-            const timeDelta = currentTime - time; // In milliseconds
+        if (!img) return
 
-            const angle = Math.asin(Math.sin(timeDelta - (0.5 / frequency) * (frequency * Math.PI) + (Math.PI / 2))) * (2 / Math.PI);
+        if (!isRunning) return
 
-            setRads(angle * (Math.PI / 4));
-         }
-    }, [isRunning, frequency, rads]);
+        const currentTime = Date.now();
+
+        // IDK where the issue is, but when bpm is 120 it looks "normal"
+        const frequency = bpm / 60.0;
+        const timeDelta = currentTime - time;
+
+        const adjustFrequency = frequency * Math.PI;
+        const clampAmplitude = 2.0 / Math.PI;
+
+        const angle = Math.asin(Math.sin(timeDelta * adjustFrequency)) * clampAmplitude;
+
+       // Play sound when angle goes from positive to negative
+
+        const rads = angle * (Math.PI / 4);
+
+        img.style.transform = `rotate(${rads}rad)`
+    }, [bpm, isRunning])
 
     // Handle BPM input change
     const handleBpmChange = (event: CustomEvent) => {
@@ -53,15 +98,24 @@ const MetronomeContainer: React.FC<ContainerProps> = ({ name }) => {
                 }
             }>
                 <div style={{ display: 'flex', justifyContent: 'center', width: '100%' }}>
+                     <img
+                        src={`${metronomeBase}`}
+                        alt="Metronome"
+                        style={
+                            {
+                                position: `relative`
+                            }
+                        }
+                    />
                     <img
                         src={`${metronomeArm}`}
                         alt="Metronome Arm"
+                        ref={armRef}
                         style={
                             {
-                                maxWidth: '90%',
-                                maxHeight: '50vh',
-                                width: 'auto',
-                                transform: `rotate(${rads}rad)`
+                                position: `absolute`,
+                                top: `0`,
+                                bottom: `0`
                             }
                         }/>
                 </div>

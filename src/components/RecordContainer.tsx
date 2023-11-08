bd4bead86b9f6af1f7d2b5849ff848b61f3c4be5
Tyler Drover <119035717+tddrover@users.noreply.github.com>
2023-11-08 11:07:39 -0800

    Add files via upload

--- src/components/RecordContainer.tsx
+++ src/components/RecordContainer.tsx
@@ -1,18 +1,18 @@
 import "./RecordContainer.css"
 
 import { useState, useRef } from "react";
-import { FaBeer } from 'react-icons/fa';
-import { CommonModule } from "@angular/common";
-import { NgModule } from '@angular/core';
+//import { FaBeer } from 'react-icons/fa';
+//import { CommonModule } from "@angular/common";
+//import { NgModule } from '@angular/core';
 import { IonButton, IonButtons, IonContent, IonInput, IonItem, IonLabel, IonToolbar, IonIcon } from '@ionic/react';
 import { download } from 'ionicons/icons';
 
 interface ContainerProps {
-  name: string;
+    name: string;
 }
 const mimeType = 'audio/mpeg';
 
-const RecordContainer: React.FC<ContainerProps> = ({ name })  => {
+const RecordContainer: React.FC<ContainerProps> = ({ name }) => {
     const [permission, setPermission] = useState(false);
     const [stream, setStream] = useState(null);
     const mediaRecorder = useRef(null);
@@ -30,7 +30,7 @@ const RecordContainer: React.FC<ContainerProps> = ({ name })  => {
                 });
                 setPermission(true);
                 setStream(streamData);
-                let isGranted:boolean = true;
+                let isGranted: boolean = true;
             } catch (err) {
                 alert(err.message);
             }
@@ -40,91 +40,91 @@ const RecordContainer: React.FC<ContainerProps> = ({ name })  => {
     };
 
     const startRecording = async () => {
-      setRecordingStatus("recording");
-      //create new Media recorder instance using the stream
-      const media = new MediaRecorder(stream);
-      //set the MediaRecorder instance to the mediaRecorder ref
-      mediaRecorder.current = media;
-      //invokes the start method to start the recording process
-      mediaRecorder.current.start();
+        setRecordingStatus("recording");
+        //create new Media recorder instance using the stream
+        const media = new MediaRecorder(stream);
+        //set the MediaRecorder instance to the mediaRecorder ref
+        mediaRecorder.current = media;
+        //invokes the start method to start the recording process
+        mediaRecorder.current.start();
 
-      let localAudioChunks = [];
+        let localAudioChunks = [];
 
-      mediaRecorder.current.ondataavailable = (event) => {
-         if (typeof event.data === "undefined") return;
-         if (event.data.size === 0) return;
-         localAudioChunks.push(event.data);
-      };
-      setAudioChunks(localAudioChunks);
-/*       else
-      {
-        alert("Cannot continue without microphone permissions");
-      } */
+        mediaRecorder.current.ondataavailable = (event) => {
+            if (typeof event.data === "undefined") return;
+            if (event.data.size === 0) return;
+            localAudioChunks.push(event.data);
+        };
+        setAudioChunks(localAudioChunks);
+        /*       else
+              {
+                alert("Cannot continue without microphone permissions");
+              } */
     };
 
     const stopRecording = () => {
-      //const i = 0;
-      setRecordingStatus("inactive");
-      //stops the recording instance
-      mediaRecorder.current.stop();
+        //const i = 0;
+        setRecordingStatus("inactive");
+        //stops the recording instance
+        mediaRecorder.current.stop();
 
-      mediaRecorder.current.onstop = () => {
-        //creates a blob file from the audiochunks data
-         const audioBlob = new Blob(audioChunks, { type: mimeType });
-        //creates a playable URL from the blob file.
-         const audioUrl = URL.createObjectURL(audioBlob);
-   /*       await Filesystem.writeFile({
-            path: audioUrl,
-            directory: Directory.Data,
-            data: audioBlob;
-         }) */
-         //array[0] = audioUrl;
-         setAudio(audioUrl);
-         setAudioChunks([]);
-         //array[0] = audioUrl;
-      };
+        mediaRecorder.current.onstop = () => {
+            //creates a blob file from the audiochunks data
+            const audioBlob = new Blob(audioChunks, { type: mimeType });
+            //creates a playable URL from the blob file.
+            const audioUrl = URL.createObjectURL(audioBlob);
+            /*       await Filesystem.writeFile({
+                     path: audioUrl,
+                     directory: Directory.Data,
+                     data: audioBlob;
+                  }) */
+            //array[0] = audioUrl;
+            setAudio(audioUrl);
+            setAudioChunks([]);
+            //array[0] = audioUrl;
+        };
     };
     getMicrophonePermission();
     /* Needs to be migrated over to Ionic Style Tags rather than HTML Style Tags */
     return (
-          <IonContent>
-                <div className="audio-controls">
-                    <img id="object"
-                        src='./resources/button.png'
-                    />
-   {/*                  <button onClick={getMicrophonePermission} type="button" aria-label="Get Microphone Permission">
+        <IonContent>
+            <div className="audio-controls">
+                <img id="object"
+                    src='./resources/button.png'
+                />
+                {/*                  <button onClick={getMicrophonePermission} type="button" aria-label="Get Microphone Permission">
                         Get Microphone Permissions
                     </button> */}
-{/*                     <button onClick={() => {getMicrophonePermission(); startRecording(); }} type="button"> */}
+                {/*                     <button onClick={() => {getMicrophonePermission(); startRecording(); }} type="button"> */}
 
-                    <IonButton onClick=
-                        {startRecording}
-                        >
-                        Start Recording
-                    </IonButton>
-                    {recordingStatus === "recording" ? (
-                        <div className="progress">
-                          <div className="progress__bar"></div>
-                        </div>
+                <IonButton onClick=
+                    {startRecording}
+                >
+                    Start Recording
+                </IonButton>
+                {recordingStatus === "recording" ? (
+                    <div className="progress">
+                        <div className="progress__bar"></div>
+                    </div>
 
-                    ) : null}
-                    <IonButton color="danger" margin-bottom="50px" onClick=
-                        {stopRecording}>
+                ) : null}
+                <IonButton color="danger" margin-bottom="50px" onClick=
+                    {stopRecording}>
 
-                        Stop Recording
-                    </IonButton>
+                    Stop Recording
+                </IonButton>
                 {audio ? (
                     <div className="audio-container">
                         <audio src={audio} controls></audio>
                         <a download href={audio}>
-                          <IonIcon className="download" icon={download}></IonIcon>
+                            <IonIcon className="download" icon={download}></IonIcon>
                         </a>
                     </div>
 
                 ) : null}
 
-                </div>
-          </IonContent>
+            </div>
+        </IonContent>
     );
 };
 
--- src/components/TunerContainer.tsx
+++ src/components/TunerContainer.tsx
@@ -1,16 +1,51 @@
-import './TunerContainer.css';
+import React, { useState } from 'react';
+import { IonContent, IonButton, IonList, IonItem, IonLabel } from '@ionic/react';
 
 interface ContainerProps {
-  name: string;
+    name: string;
 }
 
 const TunerContainer: React.FC<ContainerProps> = ({ name }) => {
-  return (
-    <div className="container">
-      <strong>{name}</strong>
-      <p>Tuner? I Hardly Know Her!</p>
-    </div>
-  );
+    const [audioFiles, setAudioFiles] = useState<File[]>([]);
+
+    // Function to load audio files from the user's selection
+    const loadAudioFiles = (event: React.ChangeEvent<HTMLInputElement>) => {
+        if (event.target.files) {
+            const selectedFiles = Array.from(event.target.files);
+            setAudioFiles(selectedFiles);
+        }
+    };
+
+    // Function to play the selected audio file
+    const playAudio = (audioFile: File) => {
+        const audioUrl = URL.createObjectURL(audioFile);
+        const audio = new Audio(audioUrl);
+        audio.play();
+    };
+
+    return (
+        <IonContent>
+            <IonButton>
+                <label htmlFor="audioFileInput">Load Audio Files</label>
+                <input
+                    type="file"
+                    id="audioFileInput"
+                    accept=".mp3" // Specify the accepted file type
+                    multiple
+                    onChange={loadAudioFiles}
+                    style={{ display: 'none' }}
+                />
+            </IonButton>
+            <IonList>
+                {audioFiles.map((audioFile, index) => (
+                    <IonItem key={index}>
+                        <IonLabel>{audioFile.name}</IonLabel>
+                        <IonButton onClick={() => playAudio(audioFile)}>Play</IonButton>
+                    </IonItem>
+                ))}
+            </IonList>
+        </IonContent>
+    );
 };
 
 export default TunerContainer;
